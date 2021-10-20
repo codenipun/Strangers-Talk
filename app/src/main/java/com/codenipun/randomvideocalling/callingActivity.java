16793ea31f4a24c33a057bd053a1c5663e6a9568
nipunjain <nipunjain119@gmail.com>
2021-10-20 14:47:26 +0530

    added webView

--- app/src/main/java/com/codenipun/randomvideocalling/callingActivity.java
+++ app/src/main/java/com/codenipun/randomvideocalling/callingActivity.java
@@ -1,9 +1,14 @@
 package com.codenipun.randomvideocalling;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 
+import com.codenipun.randomvideocalling.Models.javaInterfaces;
 import com.codenipun.randomvideocalling.databinding.ActivityCallingBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
@@ -15,5 +20,26 @@ public class callingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityCallingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    void setUpWebView(){
+        binding.webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                super.onPermissionRequest(request);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    request.grant(request.getResources());
+                }
+            }
+        });
+
+
+        //enabling javascript and disabling media gesture of webView
+        binding.webView.getSettings().setJavaScriptEnabled(true);
+        binding.webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        binding.webView.addJavascriptInterface(new javaInterfaces(this),"Android");
+
+
     }
 }
