c60cc02256db57d8f8e40f8513279418b9f44224
nipunjain <nipunjain119@gmail.com>
2021-11-09 22:08:09 +0530

    Updated rewarded Activity

--- app/src/main/java/com/codenipun/randomvideocalling/RewardedAdsActivity.java
+++ app/src/main/java/com/codenipun/randomvideocalling/RewardedAdsActivity.java
@@ -1,19 +1,45 @@
 package com.codenipun.randomvideocalling;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.codenipun.randomvideocalling.databinding.ActivityRewardedAdsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RewardedAdsActivity extends AppCompatActivity {
     ActivityRewardedAdsBinding binding;
-
+    FirebaseDatabase firebaseDatabase;
+    String currentUid;
+    int coins;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityRewardedAdsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        currentUid = FirebaseAuth.getInstance().getUid();
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        firebaseDatabase.getReference().child("Profiles").child(currentUid).child("coins")
+                .addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+               coins = snapshot.getValue(Integer.class);
+               binding.currentCoins.setText(String.valueOf(coins));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
 
 
     }
